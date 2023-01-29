d4b03b811e0508e5330875e3efd3f010f65a25f1
Muhammad Mursyid <muhdmursyid@outlook.com>
2023-01-29 13:05:05 +0800

    initial commit

---
+++ LMSLibrary/src/entity/Book.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author muhdm
+ */
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @Column(nullable = false, length = 64)
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String title;
+
+    @Column(nullable = false, length = 64)
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String isbn;
+
+    @Column(nullable = false, length = 255)
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String author;
+
+    //Relationship attributes
+    @OneToMany(mappedBy = "bookId")
+    private List<LendAndReturn> lendAndReturns;
+
+    public Book() {
+        this.lendAndReturns = new ArrayList<>();
+    }
+
+    public Book(Long bookId, String title, String isbn, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+        this.lendAndReturns = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<LendAndReturn> getLendAndReturns() {
+        return lendAndReturns;
+    }
+
+    public void setLendAndReturns(List<LendAndReturn> lendAndReturns) {
+        this.lendAndReturns = lendAndReturns;
+    }
+
+    public void addLendAndReturns(LendAndReturn lendAndReturn) {
+        this.lendAndReturns.add(lendAndReturn);
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (bookId != null ? bookId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the bookId fields are not set
+        if (!(object instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) object;
+        if ((this.bookId == null && other.bookId != null) || (this.bookId != null && !this.bookId.equals(other.bookId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Book[ id=" + bookId + " ]";
+    }
+
+}
---
+++ LMSLibrary/src/entity/Member.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author muhdm
+ */
+@Entity
+public class Member implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, length = 32)
+    @NotNull
+    @Size(min = 1, max = 32)
+    private String firstName;
+
+    @Column(nullable = false, length = 32)
+    @NotNull
+    @Size(min = 1, max = 32)
+    private String lastName;
+
+    //other attributes
+    @Column(nullable = false)
+    @NotNull
+    private Character gender;
+
+    @Column(nullable = false)
+    @NotNull
+    private Integer age;
+
+    @Column(nullable = false, unique = true, length = 32)
+    @NotNull
+    @Size(min = 9, max = 32)
+    private String identityNo;
+
+    @Column(nullable = false, unique = true, length = 32)
+    @NotNull
+    @Size(min = 8, max = 32)
+    private String phone;
+
+    @Column(nullable = false, length = 255)
+    @NotNull
+    @Size(min = 1)
+    private String address;
+
+    //Relationship attribute
+    @OneToMany(mappedBy = "memberId")
+    private List<LendAndReturn> lendAndReturns;
+
+    public Member() {
+        this.lendAndReturns = new ArrayList<>();
+    }
+
+    public Member(Long memberId, String firstName, String lastName, Character gender, Integer age, String identityNo, String phone, String address) {
+        this.memberId = memberId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.identityNo = identityNo;
+        this.phone = phone;
+        this.address = address;
+        this.lendAndReturns = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<LendAndReturn> getLendAndReturns() {
+        return lendAndReturns;
+    }
+
+    public void setLendAndReturns(List<LendAndReturn> lendAndReturns) {
+        this.lendAndReturns = lendAndReturns;
+    }
+
+    public void addLendAndReturns(LendAndReturn lendAndReturn) {
+        this.lendAndReturns.add(lendAndReturn);
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getIdentityNo() {
+        return identityNo;
+    }
+
+    public void setIdentityNo(String identityNo) {
+        this.identityNo = identityNo;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (memberId != null ? memberId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the memberId fields are not set
+        if (!(object instanceof Member)) {
+            return false;
+        }
+        Member other = (Member) object;
+        if ((this.memberId == null && other.memberId != null) || (this.memberId != null && !this.memberId.equals(other.memberId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Member[ id=" + memberId + " ]";
+    }
+
+}
