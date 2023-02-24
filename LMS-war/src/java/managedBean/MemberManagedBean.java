36bb08fee0c2ae05534caf5394005c4143d8e5fc
Muhammad Mursyid <muhdmursyid@outlook.com>
2023-02-24 19:23:27 +0800

    Registering Member now works but only if values are valid

--- LMS-war/src/java/managedBean/MemberManagedBean.java
+++ LMS-war/src/java/managedBean/MemberManagedBean.java
@@ -6,9 +6,14 @@
 package managedBean;
 
 import ejb.session.stateless.MemberLMSSessionBeanLocal;
+import entity.Member;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import util.exception.UnknownPersistenceException;
+import util.exception.UsernameExistException;
 
 /**
  *
@@ -20,8 +25,55 @@ public class MemberManagedBean {
 
     @EJB
     private MemberLMSSessionBeanLocal memberLMSSessionBean;
+    
+    private List<Member> members;
+    
+    private List<Member> filteredMembers;
+    
+    private Member newMember;
 
     public MemberManagedBean() {
     }
+    
+    @PostConstruct
+    public void init() {
+        members = memberLMSSessionBean.retrieveAllMembers();
+        newMember = new Member();
+    }
+    
+    public String registerMember() {
+        System.out.println("register member method called");
+        try {
+            memberLMSSessionBean.createNewMember(newMember);
+            return "viewAllMembers.xhtml?faces-redirect=true";
+        } catch (UsernameExistException |UnknownPersistenceException ex) {
+            return "viewAllMembers.xhtml?faces-redirect=true";
+        }
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    public List<Member> getFilteredMembers() {
+        return filteredMembers;
+    }
+
+    public void setFilteredMembers(List<Member> filteredMembers) {
+        this.filteredMembers = filteredMembers;
+    }
+
+    public Member getNewMember() {
+        return newMember;
+    }
+
+    public void setNewMember(Member newMember) {
+        this.newMember = newMember;
+    }
+    
     
 }
