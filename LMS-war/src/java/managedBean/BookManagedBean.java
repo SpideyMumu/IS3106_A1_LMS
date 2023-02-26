cff58605d9a8f36302fde897e0ba84460cb200e5
Muhammad Mursyid <muhdmursyid@outlook.com>
2023-02-27 05:48:38 +0800

    Added search function for available books

--- LMS-war/src/java/managedBean/BookManagedBean.java
+++ LMS-war/src/java/managedBean/BookManagedBean.java
@@ -13,6 +13,8 @@ import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.Locale;
+import org.primefaces.util.LangUtils;
 
 /**
  *
@@ -26,6 +28,10 @@ public class BookManagedBean implements Serializable {
     private BookLMSSessionBeanLocal bookLMSSessionBean;
 
     private List<Book> books;
+    
+    private List<Book> availBooks;
+    
+    private List<Book> filteredBooks;
 
     public BookManagedBean() {
     }
@@ -33,7 +39,34 @@ public class BookManagedBean implements Serializable {
     @PostConstruct
     public void init() {
         books = bookLMSSessionBean.retrieveAllBooks();
+        availBooks = bookLMSSessionBean.retrievAllAvailBooks();
     }
+    
+    public void lendBook() {
+        
+    }
+    
+    // For data table global search
+    public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
+        if (LangUtils.isValueBlank(filterText)) {
+            return true;
+        }
+        
+        //System.out.println("Printing FILTER TEXT: "+ filterText);
+        long filterLong;
+        try {
+            filterLong = Long.parseLong(filterText);
+        } catch (NumberFormatException ex) {
+            filterLong = -1;
+        }
+        Book book = (Book) value;
+        return book.getTitle().toLowerCase().contains(filterText)
+                || book.getAuthor().toLowerCase().contains(filterText)
+                || book.getIsbn().contains(filterText)
+                || book.getBookId() == filterLong;
+    }
+    
 
     public List<Book> getBooks() {
         return books;
@@ -42,5 +75,23 @@ public class BookManagedBean implements Serializable {
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
+    public List<Book> getAvailBooks() {
+        return availBooks;
+    }
+
+    public void setAvailBooks(List<Book> availBooks) {
+        this.availBooks = availBooks;
+    }
+
+    public List<Book> getFilteredBooks() {
+        return filteredBooks;
+    }
+
+    public void setFilteredBooks(List<Book> filteredBooks) {
+        this.filteredBooks = filteredBooks;
+    }
+    
+    
     
 }
